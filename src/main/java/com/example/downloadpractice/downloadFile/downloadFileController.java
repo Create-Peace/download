28c9d559370416470b845af9c36ffc93c4f2d2ad
Ming <zhaomingming455@163.com>
2022-04-16 11:40:38 +0800

    fix: download error

--- src/main/java/com/example/downloadpractice/downloadFile/downloadFileController.java
+++ src/main/java/com/example/downloadpractice/downloadFile/downloadFileController.java
@@ -3,7 +3,6 @@ package com.example.downloadpractice.downloadFile;
 import com.example.downloadpractice.common.utils.PdfUtil;
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfWriter;
-import org.apache.catalina.connector.Response;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,31 +17,19 @@ import java.net.URLEncoder;
 public class downloadFileController {
 
     @GetMapping("/pdf")
-    public Response downloadPdf (HttpServletResponse response) throws IOException, DocumentException {
+    public void  downloadPdf (HttpServletResponse response) throws IOException, DocumentException {
         String filename = "测试pdf";
-
         response.setContentType("application/x-download");
         response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(filename, "UTF-8") + ".pdf");
         BufferedOutputStream os = new BufferedOutputStream(response.getOutputStream());
-
         Document document = PdfUtil.createDocument();
         PdfWriter.getInstance(document, os);
-
         document.open();
-
-        Font blackFont = PdfUtil.createFont(10, Font.NORMAL, BaseColor.BLACK);
-        Font blueFont = PdfUtil.createFont(10, Font.NORMAL, BaseColor.BLUE);
         Font bigFont = PdfUtil.createFont(14, Font.NORMAL, BaseColor.BLACK);
-        Font littleFont = PdfUtil.createFont(10, Font.NORMAL, BaseColor.BLACK);
-
         Paragraph title = PdfUtil.createParagraph("测试pdf", bigFont);
         title.setAlignment(Element.ALIGN_CENTER);
-
         document.add(title);
+        document.close();
         os.close();
-
-        return new Response();
-
-
     }
 }
